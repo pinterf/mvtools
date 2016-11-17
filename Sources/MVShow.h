310730009fdcf8c0bd6f427b7b2309f13614aebf
Pinterf <Pinterf@gmail.com>
2016-11-17 18:58:59 +0100

    Define default MULTI_INSTANCE for avs+

--- Sources/MVShow.h
+++ Sources/MVShow.h
@@ -69,6 +69,10 @@ public:
 	~MVShow();
 	PVideoFrame __stdcall GetFrame(int n, IScriptEnvironment* env);
 
+  int __stdcall SetCacheHints(int cachehints, int frame_range) override {
+    return cachehints == CACHE_GET_MTMODE ? MT_MULTI_INSTANCE : 0;
+  }
+
 	void DrawMVs(unsigned char *pDst, int nDstPitch, const unsigned char *pSrc,
                  int nSrcPitch);
 	void DrawMV(unsigned char *pDst, int nDstPitch, int scale,
