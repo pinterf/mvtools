97025aa670c8d8d03789299226546329478fcf73
Ferenc Pintér <pinterfer@gmail.com>
2016-12-03 15:21:19 +0100

    yuy2planes: cleanup

--- Sources/yuy2planes.cpp
+++ Sources/yuy2planes.cpp
@@ -21,189 +21,23 @@
 #include "commonfunctions.h"
 #include "types.h"
 
-#if !defined(_M_X64)
-#define rax	eax
-#define rbx	ebx
-#define rcx	ecx
-#define rdx	edx
-#define rsi	esi
-#define rdi	edi
-#define rbp	ebp
-#define rsp	esp
-#define movzx_int mov
-#define movsx_int mov
-#else
-#define rax	rax
-#define rbx	rbx
-#define rcx	rcx
-#define rdx	rdx
-#define rsi	rsi
-#define rdi	rdi
-#define rbp	rbp
-#define rsp	r15
-#define movzx_int movzxd
-#define movsx_int movsxd
-#endif
-
-
-
 YUY2Planes::YUY2Planes(int _nWidth, int _nHeight)
 {
-	nWidth = _nWidth;
-	nHeight = _nHeight;
-	srcPitch = (nWidth + 15) & (~15);
-	srcPitchUV = (nWidth/2 + 15) & (~15); //v 1.2.1
-	pSrc = (unsigned char*) _aligned_malloc(srcPitch*nHeight,128);   //v 1.2.1
-	pSrcU = (unsigned char*) _aligned_malloc(srcPitchUV*nHeight,128);
-	pSrcV = (unsigned char*) _aligned_malloc(srcPitchUV*nHeight,128);
-
+  nWidth = _nWidth;
+  nHeight = _nHeight;
+  srcPitch = (nWidth + 15) & (~15);
+  srcPitchUV = (nWidth / 2 + 15) & (~15); //v 1.2.1
+  pSrc = (unsigned char*)_aligned_malloc(srcPitch*nHeight, 128);   //v 1.2.1
+  pSrcU = (unsigned char*)_aligned_malloc(srcPitchUV*nHeight, 128);
+  pSrcV = (unsigned char*)_aligned_malloc(srcPitchUV*nHeight, 128);
 }
 
 YUY2Planes::~YUY2Planes()
 {
-	_aligned_free (pSrc);
-	_aligned_free (pSrcU);
-	_aligned_free (pSrcV);
+  _aligned_free(pSrc);
+  _aligned_free(pSrcU);
+  _aligned_free(pSrcV);
 }
-
-//----------------------------------------------------------------------------------------------
-// v1.2.1 asm code borrowed from AviSynth 2.6 CVS
-// ConvertPlanar (c) 2005 by Klaus Post
-#if 0
-void YUY2Planes::convYUV422to422(const unsigned char *src, unsigned char *py, unsigned char *pu, unsigned char *pv,
-                                        int pitch1, int pitch2y, int pitch2uv, int width, int height)
-{
-int widthdiv2 = width>>1;
-#if !(defined(_M_X64) && defined(_MSC_VER))
-// inline asm ignored for MSVC X64 build
-// todo: get code from avs+ project
-__int64 Ymask = 0x00FF00FF00FF00FFi64;
-__asm
-{
-#if !defined(_M_X64)
-#define pitch1_ pitch1
-#define pitch2y_ pitch2y
-#define pitch2uv_ pitch2uv
-#else
-#define pitch1_ r8
-#define pitch2y_ r9
-#define pitch2uv_ r10
-		movsx_int pitch1_,pitch1
-		movsx_int pitch2y_,pitch2y
-		movsx_int pitch2uv_,pitch2uv
-#endif
-      //push ebx
-mov rdi,[src]
-mov rbx,[py]
-mov rdx,[pu]
-mov rsi,[pv]
-mov ecx,widthdiv2
-movq mm5,Ymask
-yloop:
-xor eax,eax
-align 16
-xloop:
-movq mm0,[rdi+rax*4]   ; VYUYVYUY - 1
-movq mm1,[rdi+rax*4+8] ; VYUYVYUY - 2
-movq mm2,mm0           ; VYUYVYUY - 1
-movq mm3,mm1           ; VYUYVYUY - 2
-pand mm0,mm5           ; 0Y0Y0Y0Y - 1
-psrlw mm2,8        ; 0V0U0V0U - 1
-pand mm1,mm5           ; 0Y0Y0Y0Y - 2
-psrlw mm3,8            ; 0V0U0V0U - 2
-packuswb mm0,mm1       ; YYYYYYYY
-packuswb mm2,mm3       ; VUVUVUVU
-movntq [rbx+rax*2],mm0   ; store y
-movq mm4,mm2           ; VUVUVUVU
-pand mm2,mm5           ; 0U0U0U0U
-psrlw mm4,8            ; 0V0V0V0V
-packuswb mm2,mm2       ; xxxxUUUU
-packuswb mm4,mm4       ; xxxxVVVV
-movd [rdx+rax],mm2     ; store u
-add eax,4
-cmp eax,ecx
-movd [rsi+rax-4],mm4   ; store v
-jl xloop
-add rdi,pitch1_
-add rbx,pitch2y_
-add rdx,pitch2uv_
-add rsi,pitch2uv_
-dec height
-jnz yloop
-sfence
-emms
-      //pop ebx
-#undef pitch1_
-#undef pitch2y_
-#undef pitch2uv_
-}
-#endif
-}
-#endif
-
-// YV16 -> YUY2
-#if 0
-void YUY2Planes::conv422toYUV422(const unsigned char *py, const unsigned char *pu, const unsigned char *pv,
-                           unsigned char *dst, int pitch1Y, int pitch1UV, int pitch2, int width, int height)
-{
-#if !(defined(_M_X64) && defined(_MSC_VER))
-  // inline asm ignored for MSVC X64 build
-  // todo: get code from avs+ project
-
-int widthdiv2 = width >> 1;
-__asm
-{
-#if !defined(_M_X64)
-#define pitch1Y_ pitch1Y
-#define pitch2_ pitch2
-#define pitch1UV_ pitch1UV
-#else
-#define pitch1Y_ r8
-#define pitch2_ r9
-#define pitch1UV_ r10
-		movsx_int pitch1Y_,pitch1Y
-		movsx_int pitch2_,pitch2
-		movsx_int pitch1UV_,pitch1UV
-#endif
-      //push ebx
-mov rbx,[py]
-mov rdx,[pu]
-mov rsi,[pv]
-mov rdi,[dst]
-mov ecx,widthdiv2
-yloop:
-xor eax,eax
-align 16
-xloop:
-movd mm1,[rdx+rax]     ;0000UUUU
-movd mm2,[rsi+rax]     ;0000VVVV
-movq mm0,[rbx+rax*2]   ;YYYYYYYY
-punpcklbw mm1,mm2      ;VUVUVUVU
-movq mm3,mm0           ;YYYYYYYY
-punpcklbw mm0,mm1      ;VYUYVYUY
-add eax,4
-punpckhbw mm3,mm1      ;VYUYVYUY
-movntq [rdi+rax*4-16],mm0 ;store ; bug fixed in v1.2.2, replaced by faster movntq in 2.5.1
-cmp eax,ecx
-movntq [rdi+rax*4-8],mm3   ;store  ; bug fixed in v1.2.2
-jl xloop
-add rbx,pitch1Y_
-add rdx,pitch1UV_
-add rsi,pitch1UV_
-add rdi,pitch2_
-dec height
-jnz yloop
-sfence
-emms
-      //pop ebx
-#undef pitch1Y_
-#undef pitch2Y_
-#undef pitch2UV_
-}
-#endif
-}
-#endif
-//----------------------------------------------------------------------------------------------
 
 // borrowed from Avisynth+ project
 static void convert_yuy2_to_yv16_sse2(const BYTE *srcp, BYTE *dstp_y, BYTE *dstp_u, BYTE *dstp_v, size_t src_pitch, size_t dst_pitch_y, size_t dst_pitch_uv, size_t width, size_t height)
@@ -241,12 +75,12 @@ static void convert_yuy2_to_yv16_c(const BYTE *srcp, BYTE *dstp_y, BYTE *dstp_u,
 {
   width /= 2;
 
-  for (size_t y = 0; y < height; ++y) { 
+  for (size_t y = 0; y < height; ++y) {
     for (size_t x = 0; x < width; ++x) {
-      dstp_y[x * 2]     = srcp[x * 4 + 0];
+      dstp_y[x * 2] = srcp[x * 4 + 0];
       dstp_y[x * 2 + 1] = srcp[x * 4 + 2];
-      dstp_u[x]         = srcp[x * 4 + 1];
-      dstp_v[x]         = srcp[x * 4 + 3];
+      dstp_u[x] = srcp[x * 4 + 1];
+      dstp_v[x] = srcp[x * 4 + 3];
     }
     srcp += src_pitch;
     dstp_y += dst_pitch_y;
@@ -259,10 +93,10 @@ void convert_yv16_to_yuy2_sse2(const BYTE *srcp_y, const BYTE *srcp_u, const BYT
 {
   width /= 2;
 
-  for (size_t y=0; y<height; y++) { 
-    for (size_t x=0; x<width; x+=8) {
+  for (size_t y = 0; y < height; y++) {
+    for (size_t x = 0; x < width; x += 8) {
 
-      __m128i yy = _mm_load_si128(reinterpret_cast<const __m128i*>(srcp_y + x*2));
+      __m128i yy = _mm_load_si128(reinterpret_cast<const __m128i*>(srcp_y + x * 2));
       __m128i u = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(srcp_u + x));
       __m128i v = _mm_loadl_epi64(reinterpret_cast<const __m128i*>(srcp_v + x));
 
@@ -270,8 +104,8 @@ void convert_yv16_to_yuy2_sse2(const BYTE *srcp_y, const BYTE *srcp_u, const BYT
       __m128i yuv_lo = _mm_unpacklo_epi8(yy, uv);
       __m128i yuv_hi = _mm_unpackhi_epi8(yy, uv);
 
-      _mm_stream_si128(reinterpret_cast<__m128i*>(dstp + x*4), yuv_lo);
-      _mm_stream_si128(reinterpret_cast<__m128i*>(dstp + x*4 + 16), yuv_hi);
+      _mm_stream_si128(reinterpret_cast<__m128i*>(dstp + x * 4), yuv_lo);
+      _mm_stream_si128(reinterpret_cast<__m128i*>(dstp + x * 4 + 16), yuv_hi);
     }
 
     srcp_y += src_pitch_y;
@@ -282,12 +116,12 @@ void convert_yv16_to_yuy2_sse2(const BYTE *srcp_y, const BYTE *srcp_u, const BYT
 }
 
 void convert_yv16_to_yuy2_c(const BYTE *srcp_y, const BYTE *srcp_u, const BYTE *srcp_v, BYTE *dstp, size_t src_pitch_y, size_t src_pitch_uv, size_t dst_pitch, size_t width, size_t height) {
-  for (size_t y=0; y < height; y++) {
-    for (size_t x=0; x < width / 2; x++) {
-      dstp[x*4+0] = srcp_y[x*2];
-      dstp[x*4+1] = srcp_u[x];
-      dstp[x*4+2] = srcp_y[x*2+1];
-      dstp[x*4+3] = srcp_v[x];
+  for (size_t y = 0; y < height; y++) {
+    for (size_t x = 0; x < width / 2; x++) {
+      dstp[x * 4 + 0] = srcp_y[x * 2];
+      dstp[x * 4 + 1] = srcp_u[x];
+      dstp[x * 4 + 2] = srcp_y[x * 2 + 1];
+      dstp[x * 4 + 3] = srcp_v[x];
     }
     srcp_y += src_pitch_y;
     srcp_u += src_pitch_uv;
@@ -298,105 +132,32 @@ void convert_yv16_to_yuy2_c(const BYTE *srcp_y, const BYTE *srcp_u, const BYTE *
 
 // YUY2 -> YV16
 void YUY2ToPlanes(const unsigned char *pSrcYUY2, int nSrcPitchYUY2, int nWidth, int nHeight,
-							  const unsigned char * dstY, int dstPitchY,
-							  const unsigned char * dstU, const unsigned char * dstV, int dstPitchUV, bool mmx)
+  const unsigned char * dstY, int dstPitchY,
+  const unsigned char * dstU, const unsigned char * dstV, int dstPitchUV, bool sse2)
 {
-#if 1
-  /*
-	unsigned char * pSrc1 = (unsigned char *) dstY;
-	unsigned char * pSrcU1 = (unsigned char *) dstU;
-	unsigned char * pSrcV1 = (unsigned char *) dstV;
-*/
+
   nWidth <<= 2; // real target width
-  // mmx means SSE2 here. Todo: kill mmx everywhere
-  if (mmx && IsPtrAligned(pSrcYUY2, 16)) {
-    convert_yuy2_to_yv16_sse2(pSrcYUY2, (unsigned char *) dstY, (unsigned char *) dstU, (unsigned char *) dstV, nSrcPitchYUY2, dstPitchY, dstPitchUV, nWidth, nHeight);
-} 
+  if (sse2 && IsPtrAligned(pSrcYUY2, 16)) {
+    convert_yuy2_to_yv16_sse2(pSrcYUY2, (unsigned char *)dstY, (unsigned char *)dstU, (unsigned char *)dstV, nSrcPitchYUY2, dstPitchY, dstPitchUV, nWidth, nHeight);
+  }
   else
-    {
-      convert_yuy2_to_yv16_c(pSrcYUY2, (unsigned char *) dstY, (unsigned char *) dstU, (unsigned char *) dstV, nSrcPitchYUY2, dstPitchY, dstPitchUV, nWidth, nHeight);
-    }
-#else
-  // old YUY2->YV16
-	unsigned char * pSrc1 = (unsigned char *) dstY;
-	unsigned char * pSrcU1 = (unsigned char *) dstU;
-	unsigned char * pSrcV1 = (unsigned char *) dstV;
-
-	const int awidth = std::min(nSrcPitchYUY2>>1, (nWidth+7) & -8);
-#if !(defined(_M_X64) && defined(_MSC_VER))
-  // disable non working x64 msvc asm branch
-  mmx = false;
-#endif
-//mmx=false;
-	if (!(awidth&7) && mmx) {  // Use MMX
-
-		YUY2Planes::convYUV422to422(pSrcYUY2, pSrc1, pSrcU1, pSrcV1, nSrcPitchYUY2, dstPitchY, dstPitchUV,  awidth, nHeight);
-	}
-	else
-	{
-
-	for (int h=0; h<nHeight; h++)
-	{
-		for (int w=0; w<nWidth; w+=2)
-		{
-			int w2 = w+w;
-			pSrc1[w] = pSrcYUY2[w2];
-			pSrcU1[w>>1] = pSrcYUY2[w2+1];
-			pSrc1[w+1] = pSrcYUY2[w2+2];
-			pSrcV1[w>>1] = pSrcYUY2[w2+3];
-		}
-		pSrc1 += dstPitchY;
-		pSrcU1 += dstPitchUV;
-		pSrcV1 += dstPitchUV;
-		pSrcYUY2 += nSrcPitchYUY2;
-	}
-	}
-#endif
+  {
+    convert_yuy2_to_yv16_c(pSrcYUY2, (unsigned char *)dstY, (unsigned char *)dstU, (unsigned char *)dstV, nSrcPitchYUY2, dstPitchY, dstPitchUV, nWidth, nHeight);
+  }
 }
 
 // YV16 -> YUY2
 void YUY2FromPlanes(unsigned char *pDstYUY2, int nDstPitchYUY2, int nWidth, int nHeight,
-							  unsigned char * srcY, int srcPitch,
-							  unsigned char * srcU, unsigned char * srcV, int srcPitchUV, bool mmx)
+  unsigned char * srcY, int srcPitch,
+  unsigned char * srcU, unsigned char * srcV, int srcPitchUV, bool sse2)
 {
-#if 1
-  // mmx means SSE2 here. Todo: kill mmx everywhere
-  if (mmx && IsPtrAligned(srcY, 16)) {
+  if (sse2 && IsPtrAligned(srcY, 16)) {
     //U and V don't have to be aligned since we user movq to read from those
     convert_yv16_to_yuy2_sse2(srcY, srcU, srcV, pDstYUY2, srcPitch, srcPitchUV, nDstPitchYUY2, nWidth, nHeight);
-  } else
-    {
-      convert_yv16_to_yuy2_c(srcY, srcU, srcV, pDstYUY2, srcPitch, srcPitchUV, nDstPitchYUY2, nWidth, nHeight);
-    }
-
-#else
-	const int awidth = std::min(srcPitch, (nWidth+7) & -8);
-#if !(defined(_M_X64) && defined(_MSC_VER))
-  // disable non working x64 msvc asm branch
-  mmx = false;
-#endif
-  if (!(awidth&7) && mmx) {  // Use MMX
-    YUY2Planes::conv422toYUV422(srcY, srcU, srcV, pDstYUY2, srcPitch, srcPitchUV, nDstPitchYUY2, awidth, nHeight);
   }
   else
   {
-
-  for (int h=0; h<nHeight; h++)
-	{
-		for (int w=0; w<nWidth; w+=2)
-		{
-			int w2 = w+w;
-			pDstYUY2[w2] = srcY[w];
-			pDstYUY2[w2+1] = srcU[w>>1];
-			pDstYUY2[w2+2] = srcY[w+1];
-			pDstYUY2[w2+3] = srcV[w>>1];
-		}
-		srcY += srcPitch;
-		srcU += srcPitchUV;
-		srcV += srcPitchUV;
-		pDstYUY2 += nDstPitchYUY2;
-	}
+    convert_yv16_to_yuy2_c(srcY, srcU, srcV, pDstYUY2, srcPitch, srcPitchUV, nDstPitchYUY2, nWidth, nHeight);
   }
-#endif
 }
 
