14f8c681f3e5fdc85f25d25383469472d744f9ec
Pinterf <pinterfer@gmail.com>
2017-05-14 17:38:59 +0200

    fftwlite.h remove unused defs, make similar to upstream 1.10

--- Sources/fftwlite.h
+++ Sources/fftwlite.h
@@ -1,5 +1,4 @@
 // Lite version of fftw header
-// Copyright(c)2005, Fizick
 // fftwf typedefs added in v.1.2 for delayed loading
 // 
 #ifndef __FFTWLITE_H__
@@ -14,9 +13,5 @@ typedef void (*fftwf_destroy_plan_proc) (fftwf_plan);
 typedef void (*fftwf_execute_dft_r2c_proc) (fftwf_plan, float *realdata, fftwf_complex *fftsrc);
 typedef void (*fftwf_execute_dft_c2r_proc) (fftwf_plan, fftwf_complex *fftsrc, float *realdata);
 #define FFTW_ESTIMATE (1U << 6)
-#define FFTW_REDFT01 4 /* idct */
-#define FFTW_REDFT10 5 /* dct */
-typedef fftwf_plan (*fftwf_plan_r2r_2d_proc) (int nx, int ny, float *in, float *out, int kindx, int kindy, unsigned flags);
-typedef void (*fftwf_execute_r2r_proc) (const fftwf_plan plan, float *in, float *out);
 
 #endif
